7d1d0bcec888725874c5a11d1988bb9bf4b592be
Hoangthuy125 <thuy94.mta@gmail.com>
2019-08-26 09:55:42 +0700

    thuy

--- QuanLyNhanSu/frmHSNV.Designer.cs
+++ QuanLyNhanSu/frmHSNV.Designer.cs
@@ -28,10 +28,133 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmHSNV";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmHSNV));
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtDC_HSNV = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.txtNS_HSNV = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.txtMaNV_HSNV = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.txtBC_HSNV = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.txtGT_HSNV = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.txtPB_HSNV = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMaNVNV_HSNV = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.labelX1 = new DevComponents.DotNetBar.LabelX();
+            this.reflectionImage1 = new DevComponents.DotNetBar.Controls.ReflectionImage();
+            this.reflectionImage2 = new DevComponents.DotNetBar.Controls.ReflectionImage();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.groupBox2.Controls.Add(this.groupBox1);
+            this.groupBox2.Location = new System.Drawing.Point(283, 103);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(565, 389);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.groupBox1.Controls.Add(this.txtDC_HSNV);
+            this.groupBox1.Controls.Add(this.txtNS_HSNV);
+            this.groupBox1.Controls.Add(this.txtMaNV_HSNV);
+            this.groupBox1.Controls.Add(this.txtBC_HSNV);
+            this.groupBox1.Controls.Add(this.txtGT_HSNV);
+            this.groupBox1.Controls.Add(this.txtPB_HSNV);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(30, 31);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(500, 319);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            // 
+            // txtDC_HSNV
+            // 
+            // 
+            // 
+            // 
+            this.txtDC_HSNV.Border.Class = "TextBoxBorder";
+            this.txtDC_HSNV.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.txtDC_HSNV.Location = new System.Drawing.Point(200, 231);
+            this.txtDC_HSNV.Name = "txtDC_HSNV";
+            this.txtDC_HSNV.Size = new System.Drawing.Size(212, 20);
+            this.txtDC_HSNV.TabIndex = 14;
+            // 
+            // txtNS_HSNV
+            // 
+            // 
+            // 
+            // 
+            this.txtNS_HSNV.Border.Class = "TextBoxBorder";
+            this.txtNS_HSNV.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.txtNS_HSNV.Location = new System.Drawing.Point(200, 193);
+            this.txtNS_HSNV.Name = "txtNS_HSNV";
+            this.txtNS_HSNV.Size = new System.Drawing.Size(212, 20);
+            this.txtNS_HSNV.TabIndex = 13;
+            // 
+            // txtMaNV_HSNV
+            // 
+            // 
+            // 
+            // 
+            this.txtMaNV_HSNV.Border.Class = "TextBoxBorder";
+            this.txtMaNV_HSNV.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.txtMaNV_HSNV.Location = new System.Drawing.Point(200, 77);
+            this.txtMaNV_HSNV.Name = "txtMaNV_HSNV";
+            this.txtMaNV_HSNV.Size = new System.Drawing.Size(212, 20);
+            this.txtMaNV_HSNV.TabIndex = 12;
+            // 
+            // txtBC_HSNV
+            // 
+            // 
+            // 
+            // 
+            this.txtBC_HSNV.Border.Class = "TextBoxBorder";
+            this.txtBC_HSNV.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.txtBC_HSNV.Location = new System.Drawing.Point(200, 115);
+            this.txtBC_HSNV.Name = "txtBC_HSNV";
+            this.txtBC_HSNV.Size = new System.Drawing.Size(212, 20);
+            this.txtBC_HSNV.TabIndex = 11;
+            // 
+            // txtGT_HSNV
+            // 
+            // 
+            // 
+            // 
+            this.txtGT_HSNV.Border.Class = "TextBoxBorder";
+            this.txtGT_HSNV.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.txtGT_HSNV.Location = new System.Drawing.Point(200, 155);
+            this.txtGT_HSNV.Name = "txtGT_HSNV";
+            this.txtGT_HSNV.Size = new System.Drawing.Size(212, 20);
+            this.txtGT_HSNV.TabIndex = 10;
+            // 
+            // txtPB_HSNV
+            // 
+            // 
+            // 
+            // 
+            this.txtPB_HSNV.Border.Class = "TextBoxBorder";
+            this.txtPB_HSNV.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.txtPB_HSNV.Location = new System.Drawing.Point(200, 36);
+            this.txtPB_HSNV.Name = "txtPB_HSNV";
+            this.txtPB_HSNV.Size = new System.Drawing.Size(212, 20);
+            this.txtPB_HSNV.TabIndex = 9;
+
         }
 
         #endregion
