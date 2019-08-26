a0a38c766ef85b94454bb9ae9696d1559a449f69
BuiThanhHoang <54828059+BuiThanhHoang@users.noreply.github.com>
2019-08-26 09:02:27 +0700

    update chucvu

--- QuanLyNhanSu/frmChucVu.Designer.cs
+++ QuanLyNhanSu/frmChucVu.Designer.cs
@@ -28,10 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmChucVu";
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.labelX3 = new DevComponents.DotNetBar.LabelX();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dgvCV = new DevComponents.DotNetBar.Controls.DataGridViewX();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtTimKiem = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.btnTrolai = new DevComponents.DotNetBar.ButtonX();
+            this.labTimKiem = new DevComponents.DotNetBar.LabelX();
+            this.txtTenCV = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.txtMaCV = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.labelX2 = new DevComponents.DotNetBar.LabelX();
+            this.labelX1 = new DevComponents.DotNetBar.LabelX();
+            this.btnXoaCV = new DevComponents.DotNetBar.ButtonX();
+            this.btnHienThi = new DevComponents.DotNetBar.ButtonX();
+            this.btnSuaCV = new DevComponents.DotNetBar.ButtonX();
+            this.btnThemCV = new DevComponents.DotNetBar.ButtonX();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCV)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelX3
+            // 
+            // 
+            // 
+            // 
+            this.labelX3.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX3.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelX3.ForeColor = System.Drawing.Color.DarkGreen;
+            this.labelX3.Location = new System.Drawing.Point(357, -1);
+            this.labelX3.Name = "labelX3";
+            this.labelX3.Size = new System.Drawing.Size(134, 23);
+            this.labelX3.TabIndex = 10;
+            this.labelX3.Text = "CHỨC VỤ";
+            this.labelX3.Click += new System.EventHandler(this.labelX3_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.groupBox3.Controls.Add(this.groupBox2);
+            this.groupBox3.Controls.Add(this.groupBox1);
+            this.groupBox3.Location = new System.Drawing.Point(37, 62);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(787, 432);
+            this.groupBox3.TabIndex = 11;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Enter += new System.EventHandler(this.groupBox3_Enter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.groupBox2.Controls.Add(this.dgvCV);
+            this.groupBox2.Location = new System.Drawing.Point(430, 19);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(337, 393);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
         }
 
         #endregion
