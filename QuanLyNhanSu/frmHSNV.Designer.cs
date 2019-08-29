16a487373194c788577c80f8fc720a44a8d9474f
Hoangthuy125 <thuy94.mta@gmail.com>
2019-08-29 09:56:39 +0700

    thuy

--- QuanLyNhanSu/frmHSNV.Designer.cs
+++ QuanLyNhanSu/frmHSNV.Designer.cs
@@ -154,7 +154,146 @@
             this.txtPB_HSNV.Name = "txtPB_HSNV";
             this.txtPB_HSNV.Size = new System.Drawing.Size(212, 20);
             this.txtPB_HSNV.TabIndex = 9;
-
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(40, 231);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Địa chỉ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(40, 193);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Ngày sinh";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Giới tính";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Bằng cấp";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Mã nhân viên";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(40, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Phòng ban";
+            // 
+            // txtMaNVNV_HSNV
+            // 
+            // 
+            // 
+            // 
+            this.txtMaNVNV_HSNV.Border.Class = "TextBoxBorder";
+            this.txtMaNVNV_HSNV.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.txtMaNVNV_HSNV.Location = new System.Drawing.Point(47, 258);
+            this.txtMaNVNV_HSNV.Name = "txtMaNVNV_HSNV";
+            this.txtMaNVNV_HSNV.Size = new System.Drawing.Size(177, 20);
+            this.txtMaNVNV_HSNV.TabIndex = 13;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(887, 24);
+            this.menuStrip1.TabIndex = 14;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // labelX1
+            // 
+            // 
+            // 
+            // 
+            this.labelX1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelX1.Image = global::QuanLiNhanSu.Properties.Resources.default_document2;
+            this.labelX1.Location = new System.Drawing.Point(313, 27);
+            this.labelX1.Name = "labelX1";
+            this.labelX1.Size = new System.Drawing.Size(268, 50);
+            this.labelX1.TabIndex = 4;
+            this.labelX1.Text = "HỒ SƠ NHÂN VIÊN";
+            this.labelX1.Click += new System.EventHandler(this.labelX1_Click);
+            // 
+            // reflectionImage1
+            // 
+            this.reflectionImage1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            // 
+            // 
+            // 
+            this.reflectionImage1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.reflectionImage1.BackgroundStyle.TextAlignment = DevComponents.DotNetBar.eStyleTextAlignment.Center;
+            this.reflectionImage1.Image = global::QuanLiNhanSu.Properties.Resources._13237800_506863266163514_5707257954116519100_n;
+            this.reflectionImage1.Location = new System.Drawing.Point(78, 112);
+            this.reflectionImage1.Name = "reflectionImage1";
+            this.reflectionImage1.Size = new System.Drawing.Size(128, 128);
+            this.reflectionImage1.TabIndex = 11;
+            this.reflectionImage1.Click += new System.EventHandler(this.reflectionImage1_Click);
+            // 
+            // reflectionImage2
+            // 
+            // 
+            // 
+            // 
+            this.reflectionImage2.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.reflectionImage2.BackgroundStyle.TextAlignment = DevComponents.DotNetBar.eStyleTextAlignment.Center;
+            this.reflectionImage2.Image = ((System.Drawing.Image)(resources.GetObject("reflectionImage2.Image")));
+            this.reflectionImage2.Location = new System.Drawing.Point(313, 270);
+            this.reflectionImage2.Name = "reflectionImage2";
+            this.reflectionImage2.Size = new System.Drawing.Size(17, 8);
+            this.reflectionImage2.TabIndex = 1;
+            // 
+            // frmHSNV
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(887, 538);
+            this.Controls.Add(this.txtMaNVNV_HSNV);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.labelX1);
+            this.Controls.Add(this.reflectionImage1);
+            this.Controls.Add(this.reflectionImage2);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmHSNV";
+            this.Text = "Hồ sơ nhân viên";
+            this.Load += new System.EventHandler(this.frmHSNV_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
         #endregion
