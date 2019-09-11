3680cc91e672f32f2b47a6a2792acfc0e7199dc6
tronglong1998 <54692940+tronglong1998@users.noreply.github.com>
2019-09-11 08:35:01 +0700

    Them

---
+++ QuanLyNhanSu/Luong.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyNhanSu
+{
+    class Luong
+    {
+        private string LuongNV;
+        private string LuongThuong;
+        private string MaNV;
+        private string SoNgayTangCa;
+        private string Chichu;
+        private string TongLuong;
+        public string LuongNV1
+
+        {
+            get
+            {
+                return LuongNV;
+            }
+
+            set
+            {
+                LuongNV = value;
+            }
+        }
+
+        public string LuongThuong1
+        {
+            get
+            {
+                return LuongThuong;
+            }
+
+            set
+            {
+                LuongThuong = value;
+            }
+        }
+
+        public string MaNV1
+        {
+            get
+            {
+                return MaNV;
+            }
+
+            set
+            {
+                MaNV = value;
+            }
+        }
+
+        public string Chichu1
+        {
+            get
+            {
+                return Chichu;
+            }
+
+            set
+            {
+                Chichu = value;
+            }
+        }
+
+        public string SoNgayTangCa1
+        {
+            get
+            {
+                return SoNgayTangCa;
+            }
+
+            set
+            {
+                SoNgayTangCa = value;
+            }
+        }
+
+        public string TongLuong1
+        {
+            get
+            {
+                return TongLuong;
+            }
+
+            set
+            {
+                TongLuong = value;
+            }
+        }
+        
+        public Sua_Luong(NhanVien a,string manv, string luong, string luongthuong, string ghichu, string songaytangca, string tongluong)
+        {
+            
+            this.a.MaNV = manv;
+            this.a.LuongNV = luong;
+            this.a.LuongThuong = luongthuong;
+            this.a.Chichu = ghichu;
+            this.a.SoNgayTangCa = songaytangca;
+            this.a.TongLuong = tongluong;
+        }
+        public Them_Luong(string manv, string luong, string luongthuong, string ghichu, string songaytangca, string tongluong)
+        {
+            this.a.MaNV = manv;
+            this.a.LuongNV = luong;
+            this.a.LuongThuong = luongthuong;
+            this.a.Chichu = ghichu;
+            this.a.SoNgayTangCa = songaytangca;
+            this.a.TongLuong = tongluong;
+        }
+        public Xoa_Luong(string manv, string luong, string luongthuong, string ghichu, string songaytangca, string tongluong)
+        {
+            this.a.MaNV = manv;
+            this.a.LuongNV = 0;
+            this.a.LuongThuong = 0;
+            this.a.Chichu = '';
+            this.a.SoNgayTangCa = 0;
+            this.a.TongLuong = 0;
+        }
+        public Luong(string manv, string luong, string luongthuong, string ghichu, string songaytangca, string tongluong)
+        {
+            this.MaNV = manv;
+            this.LuongNV = luong;
+            this.LuongThuong = luongthuong;
+            this.Chichu = ghichu;
+            this.SoNgayTangCa = songaytangca;
+            this.TongLuong = tongluong;
+        }
+    }
+}
---
+++ QuanLyNhanSu/NhanVien.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyNhanSu
+{
+    class NhanVien
+    {
+        public string Manv { get; set; }
+        public string Tennv { get; set; }
+        public string Diachi { get; set; }
+        private DateTime ngaysinh;
+        private string bangcap;
+        private string gt;
+        private string mapb;
+        private string tongiao;
+        private string dantoc;
+        private string sdt;
+        private string cmt;
+
+
+
+        public string Bangcap
+        {
+            get
+            {
+                return bangcap;
+            }
+
+            set
+            {
+                bangcap = value;
+            }
+        }
+
+        public string Gt
+        {
+            get
+            {
+                return gt;
+            }
+
+            set
+            {
+                gt = value;
+            }
+        }
+        public string Macv
+        {
+            get
+         {
+               return macv;
+           }
+
+           set
+           {
+               macv = value;
+           }
+        }
+        public string Manv
+        {
+            get
+            {
+                return Manv;
+            }
+
+            set
+            {
+                Manv = value;
+            }
+        }
+        public string Tennv
+        {
+            get
+            {
+                return Tennv;
+            }
+
+            set
+            {
+                Tennv = value;
+            }
+        }
+        public string Diachi
+        {
+            get
+            {
+                return Diachi;
+            }
+
+            set
+            {
+                Diachi = value;
+            }
+        }
+        public DateTime Ngaysinh
+        {
+            get
+            {
+                return ngaysinh;
+            }
+
+            set
+            {
+                ngaysinh = value;
+            }
+        }
+
+        public string Bangcap
+        {
+            get
+            {
+                return bangcap;
+            }
+
+            set
+            {
+                bangcap = value;
+            }
+        }
+
+        public string Gt
+        {
+            get
+            {
+                return gt;
+            }
+
+            set
+            {
+                gt = value;
+            }
+        }
+
+
+
+        public string Tongiao
+        {
+            get
+            {
+                return tongiao;
+            }
+
+            set
+            {
+                tongiao = value;
+            }
+        }
+
+        public string Dantoc
+        {
+            get
+            {
+                return dantoc;
+            }
+
+            set
+            {
+                dantoc = value;
+            }
+        }
+
+        public string Sdt
+        {
+            get
+            {
+                return sdt;
+            }
+
+            set
+            {
+                sdt = value;
+            }
+        }
+
+        public string Cmt
+        {
+            get
+            {
+                return cmt;
+            }
+
+            set
+            {
+                cmt = value;
+            }
+        }
+
+        public string Mapb
+        {
+            get
+            {
+                return mapb;
+            }
+
+            set
+            {
+                mapb = value;
+            }
+        }
+
+        public Them_NhanVien(string manv, string tennv, string bangcap, string gt, DateTime ngaysinh, string diachi, string mapb, string sdt, string cmt, string dantoc, string tongiao)
+        {
+            NhanVien a = new NhanVien();
+            this.a.Manv = manv;
+            this.a.Tennv = tennv;
+            this.a.Bangcap = bangcap;
+            this.a.Gt = gt;
+            this.a.Ngaysinh = ngaysinh;
+            this.a.Diachi = diachi;
+            this.a.Mapb = mapb;
+            this.a.Tongiao = tongiao;
+            this.a.Dantoc = dantoc;
+            this.a.Sdt = sdt;
+            this.a.Cmt = cmt;
+
+        }
+        public Sua_NhanVien(NhanVien a,string manv1, string tennv1, string bangcap1, string gt1, DateTime ngaysinh1, string diachi1, string mapb1, string sdt1, string cmt1, string dantoc1, string tongiao1)
+        {
+           
+            this.a.Manv = manv1;
+            this.a.Tennv = tennv1;
+            this.a.Bangcap = bangcap1;
+            this.a.Gt = gt1;
+            this.a.Ngaysinh = ngaysinh1;
+            this.a.Diachi = diachi1;
+            this.a.Mapb = mapb1;
+            this.a.Tongiao = tongiao1;
+            this.a.Dantoc = dantoc1;
+            this.a.Sdt = sdt1;
+            this.a.Cmt = cmt1;
+
+        }
+        public Xoa_NhanVien(NhanVien a)
+        {
+
+            this.a.Manv = '';
+            this.a.Tennv = '';
+            this.a.Bangcap = '';
+            this.a.Gt = '';
+            this.a.Ngaysinh = ''; 
+            this.a.Diachi = '';
+            this.a.Mapb = '';
+            this.a.Tongiao = '';
+            this.a.Dantoc = '';
+            this.a.Sdt = '';
+            this.a.Cmt = '';
+
+        }
+        public NhanVien(string manv, string tennv, string bangcap, string gt, DateTime ngaysinh, string diachi, string mapb, string sdt, string cmt, string dantoc, string tongiao)
+        {
+            this.Manv = manv;
+            this.Tennv = tennv;
+            this.Bangcap = bangcap;
+            this.Gt = gt;
+            this.Ngaysinh = ngaysinh;
+            this.Diachi = diachi;
+            this.Mapb = mapb;
+            this.Tongiao = tongiao;
+            this.Dantoc = dantoc;
+            this.Sdt = sdt;
+            this.Cmt = cmt;
+
+        }
+    }
+}
