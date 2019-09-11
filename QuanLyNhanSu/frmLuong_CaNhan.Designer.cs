9613e8412b3157595781a64bde9ed7fb83dfd641
Hoangthuy125 <thuy94.mta@gmail.com>
2019-09-11 10:11:42 +0700

    thuy

--- QuanLyNhanSu/frmLuong_CaNhan.Designer.cs
+++ QuanLyNhanSu/frmLuong_CaNhan.Designer.cs
@@ -28,10 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmLuong_CaNhan";
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.labelX1 = new DevComponents.DotNetBar.LabelX();
+            this.dgvLuong_CaNhan = new DevComponents.DotNetBar.Controls.DataGridViewX();
+            this.txtTimKiem = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.txtTenNV = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.txtLuong = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.txtLuongThuong = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.txtGhichu = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.txtMaNV = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.labelX6 = new DevComponents.DotNetBar.LabelX();
+            this.labelX5 = new DevComponents.DotNetBar.LabelX();
+            this.labelX4 = new DevComponents.DotNetBar.LabelX();
+            this.labelX3 = new DevComponents.DotNetBar.LabelX();
+            this.labelX7 = new DevComponents.DotNetBar.LabelX();
+            this.txtSNTC = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.labelX8 = new DevComponents.DotNetBar.LabelX();
+            this.txtTongLuong = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.labelX9 = new DevComponents.DotNetBar.LabelX();
+            this.btnTongLuong = new DevComponents.DotNetBar.ButtonX();
+            this.btnThuong = new DevComponents.DotNetBar.ButtonX();
+            this.labelX2 = new DevComponents.DotNetBar.LabelX();
+            this.buttonX1 = new DevComponents.DotNetBar.ButtonX();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLuong_CaNhan)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelX1
+            // 
+            // 
+            // 
+            // 
+            this.labelX1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelX1.Location = new System.Drawing.Point(467, 22);
+            this.labelX1.Name = "labelX1";
+            this.labelX1.Size = new System.Drawing.Size(290, 27);
+            this.labelX1.TabIndex = 0;
+            this.labelX1.Text = "QUẢN LÝ LƯƠNG CÁ NHÂN";
+            // 
+            // dgvLuong_CaNhan
+            // 
+            this.dgvLuong_CaNhan.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvLuong_CaNhan.BackgroundColor = System.Drawing.SystemColors.InactiveBorder;
+            this.dgvLuong_CaNhan.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvLuong_CaNhan.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvLuong_CaNhan.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(215)))), ((int)(((byte)(229)))));
+            this.dgvLuong_CaNhan.Location = new System.Drawing.Point(127, 139);
+            this.dgvLuong_CaNhan.Name = "dgvLuong_CaNhan";
+            this.dgvLuong_CaNhan.Size = new System.Drawing.Size(741, 332);
+            this.dgvLuong_CaNhan.TabIndex = 1;
+            this.dgvLuong_CaNhan.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvLuong_CaNhan_CellContentClick_1);
+            this.dgvLuong_CaNhan.SelectionChanged += new System.EventHandler(this.dgvLuong_CaNhan_SelectionChanged);
+
         }
 
         #endregion
