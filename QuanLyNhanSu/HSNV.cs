e44d550a50a266aa66259f6286f4a058fa6749af
asdcxsd <nguyenmanhthien98@gmail.com>
2019-09-13 09:29:05 +0700

    Update HSNV.cs

--- QuanLyNhanSu/HSNV.cs
+++ QuanLyNhanSu/HSNV.cs
@@ -45,34 +45,6 @@ namespace QuanLyNhanSu
 
 
         }
-		   public void HienThiDataGridViewComboBoxColumn(DataGridViewComboBoxColumn cmbColumn)
 
-        {
-            cmbColumn.DataSource = m_MonHocData.LayDsMonHoc();
-
-            cmbColumn.DisplayMember = "TenMonHoc";
-
-
-            cmbColumn.ValueMember = "MaMonHoc";
-
-            cmbColumn.DataPropertyName = "MaMonHoc";
-
-            cmbColumn.HeaderText = "Môn học";
-        }
-
-        public void HienThiDataGridViewComboBoxColumnGiaoVien(DataGridViewComboBoxColumn cmbColumn)
-        {
-
-            cmbColumn.DataSource = m_MonHocData.LayDsMonHoc();
-            cmbColumn.DisplayMember = "TenMonHoc";
-
-            cmbColumn.ValueMember = "MaMonHoc";
-
-            cmbColumn.DataPropertyName = "MaMonHoc";
-
-            cmbColumn.HeaderText = "Chuyên môn";
-
-
-        }
     }
 }
