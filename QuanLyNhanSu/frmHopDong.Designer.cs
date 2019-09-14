1c4fae1f807e9b46cd37d7144fff906f511d0839
BuiThanhHoang <54828059+BuiThanhHoang@users.noreply.github.com>
2019-09-14 21:40:38 +0700

    update formHopDong

--- QuanLyNhanSu/frmHopDong.Designer.cs
+++ QuanLyNhanSu/frmHopDong.Designer.cs
@@ -28,10 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmHopDong";
+            this.labelX1 = new DevComponents.DotNetBar.LabelX();
+            this.dgvHienThi = new System.Windows.Forms.DataGridView();
+            this.tbnXoa = new DevComponents.DotNetBar.ButtonX();
+            this.tbnThoat = new DevComponents.DotNetBar.ButtonX();
+            this.tbnGiaHan = new DevComponents.DotNetBar.ButtonX();
+            this.tbnSua = new DevComponents.DotNetBar.ButtonX();
+            this.tbnThem = new DevComponents.DotNetBar.ButtonX();
+            this.labelX2 = new DevComponents.DotNetBar.LabelX();
+            this.labelX3 = new DevComponents.DotNetBar.LabelX();
+            this.labelX4 = new DevComponents.DotNetBar.LabelX();
+            this.labelX5 = new DevComponents.DotNetBar.LabelX();
+            this.labelX6 = new DevComponents.DotNetBar.LabelX();
+            this.txtTenHD = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.txtTenNV = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.txtMaNV = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.labelX7 = new DevComponents.DotNetBar.LabelX();
+            this.txtMaHD = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.dateBD = new System.Windows.Forms.DateTimePicker();
+            this.dateKT = new System.Windows.Forms.DateTimePicker();
+            this.btnTV = new DevComponents.DotNetBar.ButtonX();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHienThi)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelX1
+            // 
+            // 
+            // 
+            // 
+            this.labelX1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelX1.Location = new System.Drawing.Point(287, 26);
+            this.labelX1.Name = "labelX1";
+            this.labelX1.Size = new System.Drawing.Size(351, 23);
+            this.labelX1.TabIndex = 0;
+            this.labelX1.Text = "HỢP ĐỒNG LÀM VIỆC CỦA NHÂN VIÊN";
+            // 
+            // dgvHienThi
+            // 
+            this.dgvHienThi.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvHienThi.BackgroundColor = System.Drawing.Color.AliceBlue;
+            this.dgvHienThi.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvHienThi.Location = new System.Drawing.Point(270, 87);
+            this.dgvHienThi.Name = "dgvHienThi";
+            this.dgvHienThi.Size = new System.Drawing.Size(655, 358);
+            this.dgvHienThi.TabIndex = 1;
+            this.dgvHienThi.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvHienThi_CellContentClick);
+            this.dgvHienThi.SelectionChanged += new System.EventHandler(this.dgvHienThi_SelectionChanged);
+            // 
+            // tbnXoa
+            // 
+            this.tbnXoa.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.tbnXoa.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.tbnXoa.Location = new System.Drawing.Point(24, 372);
+            this.tbnXoa.Name = "tbnXoa";
+            this.tbnXoa.Size = new System.Drawing.Size(75, 23);
+            this.tbnXoa.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.tbnXoa.TabIndex = 2;
+            this.tbnXoa.Text = "Xóa";
+            this.tbnXoa.Click += new System.EventHandler(this.tbnXoa_Click);
         }
 
         #endregion
