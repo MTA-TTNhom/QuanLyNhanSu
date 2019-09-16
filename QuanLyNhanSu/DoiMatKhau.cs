07969a6da16e22c0bd2280c453f8320b97a3c2e0
HuynhLeHoang <chunickdeptrailam@gmail.com>
2019-09-16 23:02:10 +0700

    Update DoiMatKhau.cs

--- QuanLyNhanSu/DoiMatKhau.cs
+++ QuanLyNhanSu/DoiMatKhau.cs
@@ -8,13 +8,60 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace QuanLyNhanSu
+namespace QuanLiNhanSu
 {
     public partial class DoiMatKhau : Form
     {
         public DoiMatKhau()
         {
             InitializeComponent();
+            txtTenDN.ReadOnly = true;
+            txtTenDN.Text = Program.usernamelogin;
+        }
+
+
+     
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            status.ForeColor = Color.Red;
+            if (ConnectDatabase.Login(txtTenDN.Text, txtmatkhaucu.Text) == false)
+            {
+                status.Text = "Sai mật khẩu!!";
+                return;
+            }
+            if (txtmatkhaumoi.Text != txtnhaplaimatkhaumoi.Text)
+            {
+                status.Text = "Mật khẩu nhập lại không trùng khớp !!";
+                return;
+            }
+            ConnectDatabase.thayDoiMatKhau(txtTenDN.Text, txtmatkhaumoi.Text);
+            status.ForeColor = Color.Blue;
+            status.Text = "Đã đổi mật khẩu!";
+        }
+
+      
+
+        private void btnThoat_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnLuu_Click_1(object sender, EventArgs e)
+        {
+            status.ForeColor = Color.Red;
+            if (ConnectDatabase.Login(txtTenDN.Text, txtmatkhaucu.Text) == false)
+            {
+                status.Text = "Sai mật khẩu!!";
+                return;
+            }
+            if (txtmatkhaumoi.Text != txtnhaplaimatkhaumoi.Text)
+            {
+                status.Text = "Mật khẩu nhập lại không trùng khớp !!";
+                return;
+            }
+            ConnectDatabase.thayDoiMatKhau(txtTenDN.Text, txtmatkhaumoi.Text);
+            status.ForeColor = Color.Blue;
+            status.Text = "Đã đổi mật khẩu!";
         }
     }
 }
