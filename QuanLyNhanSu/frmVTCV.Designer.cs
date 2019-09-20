22c32737e2991320280803be92f7137f8258f3e5
Lê Ngọc Dự <55566554+hoten1701@users.noreply.github.com>
2019-09-20 20:33:23 +0700

    add frmVTCV

---
+++ QuanLyNhanSu/frmVTCV.Designer.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyNhanSu
+{
+    public partial class frmVTCV : Form
+    {
+        public frmVTCV()
+        {
+            InitializeComponent();
+        }
+    }
+}
