5e01b6a96314205e7e8c5c4f0ba0daf1a14695c2
Lê Ngọc Dự <55566554+hoten1701@users.noreply.github.com>
2019-09-22 20:34:22 +0700

    Update frmVTCV

--- QuanLyNhanSu/frmVTCV.Designer.cs
+++ QuanLyNhanSu/frmVTCV.Designer.cs
@@ -13,5 +13,18 @@ namespace QuanLyNhanSu
         {
             InitializeComponent();
         }
+
+        private void btnThemCV_Click(object sender, EventArgs e)
+        {
+            ViTri temp = new ViTri(txtMaVT.Text, txtTenVT.Text);
+            if (ConnectDatabase.checkViTri(temp.Mavt) == 1)
+            {
+                MessageBox.Show("Mã vị trí đã tồn tại. Xin mời nhập mã khác!!!");
+                return;
+
+            }
+            ConnectDatabase.ThemViTri(temp);
+            dgvVT.DataSource = ConnectDatabase.getAllViTri();
+        }
     }
 }
