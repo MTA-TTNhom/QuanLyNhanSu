9843a2a2ef57444f24625797a53cd4ed6f5f3514
asdcxsd <nguyenmanhthien98@gmail.com>
2019-10-13 15:31:17 +0700

    doi ten

---
+++ QuanLiNhanSu/PhanCong_ViTri.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyNhanSu
+{
+    class PhanCong_ViTri
+    {
+        private string MaVT;
+        private string MaNV;
+
+
+        public string MaVT1
+        {
+            get
+            {
+                return MaVT;
+            }
+
+            set
+            {
+                MaVT = value;
+            }
+        }
+
+
+        public string MaNV1
+        {
+            get
+            {
+                return MaNV;
+            }
+
+            set
+            {
+                MaNV = value;
+            }
+        }
+        public PhanCong_ViTri(string manv, string mavt)
+        {
+            this.MaVT = mavt;
+            this.MaNV = manv;
+
+        }
+
+
+
+
+
+
+    }
+}
